53a8ec87d0ff3b2f2bf866c9ef9d5f76cc93e8df
hemanta <hemantagarg5@gmail.com>
2017-11-04 21:11:05 +0530

    Profile changes

--- app/src/main/java/com/app/skybarge/activities/DashboardHome.java
+++ app/src/main/java/com/app/skybarge/activities/DashboardHome.java
@@ -1,8 +1,11 @@
 package com.app.skybarge.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -11,7 +14,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.app.skybarge.R;
@@ -21,7 +29,9 @@ public class DashboardHome extends AppCompatActivity {
     private Activity context;
     private DrawerLayout drawer;
     private View main_view;
+    private Button btn_need_leave;
     private TextView mTvHome, mTvProfile, mTvCalendar, mTvHolidayList, mTvNewLeaves, mTvLeavePolicy;
+    private String TAG = DashboardHome.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +43,26 @@ public class DashboardHome extends AppCompatActivity {
     }
 
     private void setListner() {
-
         mTvCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, CalendarActivity.class);
                 startActivity(intent);
+            }
+        });
+        mTvProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btn_need_leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNeedLeaveDialog();
             }
         });
     }
@@ -50,7 +74,7 @@ public class DashboardHome extends AppCompatActivity {
         mTvHolidayList = (TextView) findViewById(R.id.mTvHolidayList);
         mTvNewLeaves = (TextView) findViewById(R.id.mTvNewLeaves);
         mTvLeavePolicy = (TextView) findViewById(R.id.mTvLeavePolicy);
-
+        btn_need_leave = (Button) findViewById(R.id.btn_need_leave);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Drawable drawable = ContextCompat.getDrawable(context, R.drawable.left_menu);
         toolbar.setNavigationIcon(drawable);
@@ -77,6 +101,57 @@ public class DashboardHome extends AppCompatActivity {
         toggle.syncState();
     }
 
+
+    /**
+     * Open dialog for the edit comment
+     */
+    private void openNeedLeaveDialog() {
+        try {
+            final Dialog dialog = new Dialog(context);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+            // inflate the layout dialog_layout.xml and set it as contentView
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.need_leave_dialog, null, false);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setContentView(view);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            final EditText mEdtComment = (EditText) view.findViewById(R.id.mEdtComment);
+            Button btnSubmit = (Button) view.findViewById(R.id.btn_submit);
+            Button btnCancel = (Button) view.findViewById(R.id.btn_cancel);
+
+/*
+            btnSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (!edt_comment.getText().toString().equalsIgnoreCase("")) {
+                        updateFeed(edt_comment.getText().toString(), id);
+                        dialog.dismiss();
+                    } else {
+                        edt_comment.setError("Please enter comment");
+                        edt_comment.requestFocus();
+                    }
+
+                }
+            });
+*/
+            btnCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            if (dialog != null && !dialog.isShowing()) {
+                dialog.show();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, " Exception error : " + e);
+        }
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -86,6 +161,4 @@ public class DashboardHome extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-
 }
---
+++ app/src/main/java/com/app/skybarge/activities/UserProfile.java
@@ -0,0 +1,18 @@
+package com.app.skybarge.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.app.skybarge.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+
+
+    }
+}
